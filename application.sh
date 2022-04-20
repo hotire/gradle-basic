a62dd3b91633d2c7950634febd49b58f72f1a525
hotire <gngh0101@gmail.com>
2022-04-21 01:16:42 +0900

    Implement getRevision

--- application.sh
+++ application.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
+# shellcheck disable=SC2112
 function getJarPath() {
   path="/Users/user/Documents/workspaces/monitoring/spring-actuator/build/libs"
   echo $path
   jarPath=$(find $path -name '*.jar')
+  revision=$(getRevision $jarPath)
+  echo $revision
   echo $jarPath
 }
+
+function getRevision() {
+    STR_ARRAY=(`echo $1 | tr "-" "\n"`)
+    last=${STR_ARRAY[@]:(-1)}
+    revision=(`echo $last | cut -d. -f1`)
+    echo $revision
+}
+
 
 COMMAND=$1
 
